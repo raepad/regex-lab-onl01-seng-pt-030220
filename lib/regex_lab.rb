167862cb2a7aa569d75b734af6d99a02fbe729da
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-20 19:43:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/(\w*{10})\s?\(?\)?\-?/) ? true : false
+  phone.match?(/^(\W*\d{1}){10}\W*$/)
+  #phone.scan(/(\w*{10})\s?\(?\)?\-?/) ? true : false
   #phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
   #phone.scan(/1?[\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/) ? true : false
 end
